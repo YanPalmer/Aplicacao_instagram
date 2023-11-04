5c383acb8086faf7eca75927469f829c6bab2622
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-11-04 01:26:06 -0300

    Cadastramento quase completo

---
+++ public/pages/cadastro.js
@@ -0,0 +1,36 @@
+const button = document.getElementById("cadastrar");
+button.addEventListener('click', cadastrarUsuario);
+
+async function cadastrarUsuario() {
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    try {
+        if (nome && email && senha) {
+            const response = await fetch('/cadastrarUsuario', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: nome,
+                    email: email,
+                    password: senha
+                })
+            })
+            // Parei aqui...
+
+
+        } else {
+            const camposFaltando = [];
+            if (!nome) camposFaltando.push("Nome");
+            if (!email) camposFaltando.push("Email");
+            if (!senha) camposFaltando.push("Senha");
+            const mensagemDeErro = `Campos faltando: ${camposFaltando.join(", ")}`;
+            throw new Error(mensagemDeErro);
+        }
+    } catch (error) {
+        alert(error.message)
+    }
+
+}
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import Express, { request, response } from "express";
+import { cadastrarUsuario } from "./controlers/cadastrarUsuario";
 const app = Express();
 
 app.use(Express.json());
 app.use(Express.static("public"));
+
+app.post('/cadastrarUsuario', cadastrarUsuario)
 
 export default app;
