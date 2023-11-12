9c10ab5012b081a609e63efe9457f8cca83ce72b
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-11-12 00:54:31 -0300

    Código para adicionar postagens

--- public/pages/pagina_usuario/pagina_usuario.js
+++ public/pages/pagina_usuario/pagina_usuario.js
@@ -24,4 +24,62 @@ function preencherCampos() {
         alert(`Bem vindo "${nomeUsuario.textContent}"`);
     }
 
+    // CÓDIGO PARA PEGAR AS IMAGENS DO BANCO DE DADOS
+
 }
+
+
+// Copiado do chatGPT
+// Código que cria um bloco para realizar uma postagem
+function adicionarPostagem() {
+    const adicionarPostagem = document.getElementById("adicionarPostagem");
+    adicionarPostagem.addEventListener('click', novaPostagem);
+
+    function novaPostagem() {
+        const article = document.createElement('article');
+
+        const img = document.createElement('img');
+        img.src = "../../images/sem_imagem.jpg";
+
+        const input = document.createElement('input');
+        input.type = "file";
+        input.accept = "image/*";
+        input.addEventListener('change', function () {
+            exibirImagemSelecionada(input, img);
+        });
+
+        const descricao = document.createElement('input');
+        descricao.id = "descricao";
+        descricao.placeholder = "Escreva o que está pensando";
+
+        const button = document.createElement('button');
+        button.id = "enviarPostagem";
+        button.innerHTML = "Enviar";
+
+
+        article.appendChild(img);
+        article.appendChild(input);
+        article.appendChild(descricao);
+        article.appendChild(button);
+
+        const section = document.getElementById("postagens");
+        section.appendChild(article);
+    }
+
+    function exibirImagemSelecionada(input, img) {
+        if (input.files && input.files[0]) {
+            var leitor = new FileReader();
+
+            leitor.onload = function (e) {
+                img.src = e.target.result;
+            };
+
+            leitor.readAsDataURL(input.files[0]);
+        }
+    } // Copiado do chatGPT
+}
+
+
+
+
+adicionarPostagem();
