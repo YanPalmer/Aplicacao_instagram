f2b016b8ff82f544f83e34a7e63112de66804b37
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-11-15 00:51:48 -0300

    Implementar busca de nomes
    Preciso buscar os nomes dos usuários para que eles apareçam assim que suas respectivas imagens sejam carregadas

--- public/pages/pagina_inicial/pagina_inicial.js
+++ public/pages/pagina_inicial/pagina_inicial.js
@@ -16,7 +16,26 @@ async function preencherPaginaInicial() {
     resposta.dados.forEach(element => {
         // Função que renderiza as postagens na página inicial
         // SÓ FALTA ISSO
+        // console.log(element.id, element.imagem, element.description);
 
+        const article = document.createElement('article');
+
+        const h3 = document.createElement('h3');
+        h3.innerHTML = element.id;
+
+        const img = document.createElement('img');
+        img.src = "../../images/sem_imagem.jpg";
+        // img.src = element.image;
+
+        const p = document.createElement('p');
+        p.innerHTML = element.description;
+
+        article.appendChild(h3);
+        article.appendChild(img);
+        article.appendChild(p);
+
+        const section = document.getElementById('postagens');
+        section.appendChild(article);
         
     });
 }
